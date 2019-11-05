3c81a1af3e676440740298e3825a9bc32c20d435
Acebang <493950747@qq.com>
2019-11-05 16:37:24 +0800

    2019.11.05

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <div>{{tsVal1 + tsVal2}}</div>
+    <div>{{jsVal1 + jsVal2}}</div>
   </div>
 </template>
 
-<script>
+<script lang='ts'>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import { Component, Vue, Provide } from 'vue-property-decorator'
 
-export default {
-  name: "home",
-  components: {
+@Component({
+  name:"home",
+  components:{
     HelloWorld
+  }
+})
+export default class home extends Vue{
+  tsVal1: string = "11";
+  tsVal2: string = "22";
+  data(){
+    return{
+      jsVal1:"1",
+      jsVal2:"2"
+    }
   }
 };
 </script>
